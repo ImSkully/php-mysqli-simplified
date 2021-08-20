b6f73aa3144f825b7f9739036ee2fb915b3ba105
Skully <faran.bari@hotmail.com>
2021-08-20 05:12:36 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -2,13 +2,13 @@
 
 // Replace values below respective to your database.
 define("DB_PRIMARY_HOST", "127.0.0.1"); // Database IP, if running a local server leave this alone or use 'localhost'.
-define("DB_PRIMARY_NAME", "root"); // MySQL account username, using the root account is deprecated for security reasons.
-define("DB_PRIMARY_USER", "password"); // MySQL account password.
-define("DB_PRIMARY_PASS", "database_name"); // The name of the database to connect to.
+define("DB_PRIMARY_USER", "root"); // MySQL account username, using the root account is deprecated for security reasons.
+define("DB_PRIMARY_PASS", "password"); // MySQL account password.
+define("DB_PRIMARY_NAME", "database_name"); // The name of the database to connect to.
 
 /* Add more databases as required.
-define("DB_SECONDARY_HOST", "127.0.0.1");
-define("DB_SECONDARY_NAME", "root");
-define("DB_SECONDARY_USER", "password");
-define("DB_SECONDARY_PASS", "second_database_name");
+define("DB_PRIMARY_HOST", "127.0.0.1");
+define("DB_PRIMARY_USER", "root");
+define("DB_PRIMARY_PASS", "password");
+define("DB_PRIMARY_NAME", "database_name");
 */
